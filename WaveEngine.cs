16ad622df7d4344df6a795d8c3b3c145f6f1366a
Kevan Jordan <kevanjordan9@gmail.com>
2023-04-28 20:31:53 +0100

    started working on wave simulator project

---
+++ WaveEngine.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Threading;
+using System.Windows;
+using System.Windows.Forms;
+
+public class WaveEngine {
+    Bitmap bmp;
+    BitmapData bd;
+    
+    float[] vd;
+    float[] vdv;
+    float[] vda; 
+    float[] vds; 
+}
